bab9bddd2da85bc464568d4b86e880bb480921d4
sickdyd <roberto.reale.ja@gmail.com>
2020-01-29 15:40:41 +0900

    Updated demo

--- src/App.js
+++ src/App.js
@@ -4,22 +4,14 @@ import bg from "./bg.jpg";
 import './App.css';
  
 const style = {
-  display: "flex",
-  flexDirection: "column",
-  justifyContent: "center",
-  alignItems: "center",
-  backgroundImage: `url(${bg})`,
-  backgroundSize: "contain",
-  height: "230px",
-  width: "300px",
-  fontSize: "25px",
   borderRadius: "4px",
-  boxShadow: "0 2px 5px -1px rgba(0, 0, 0, 0.3)"
+  boxShadow: "0 2px 5px -1px rgba(0, 0, 0, 0.3)",
 } 
  
 function App() {
 
   const [position, setPosition] = React.useState({x: 0, y: 0});
+  const [enabled, setEnabled] = React.useState(true);
 
   React.useState(()=>{
     let id;
@@ -37,13 +29,27 @@ function App() {
     <div className="App">
       <header className="App-header">
 
-        <span>ReactFlashlight demo by Sickdyd</span>
+        <span style={{color: "red", fontSize: "calc(2vmin + 20px)"}}>ReactFlashlight demo by Sickdyd</span>
 
         <div className="container">
-          <span>Use the <span style={{color: "red"}}>mouse</span> to move the light and the <span style={{color: "red"}}>wheel</span> to change its size.</span>
-          <ReactFlashlight>
+          <span>Menu example</span>
+          <div className="menu">
+            <ReactFlashlight showCursor>
+              <div className="btn">Home</div>
+              <div className="btn">Projects</div>
+              <div className="btn">Contacts</div>
+              <div className="btn">Something</div>
+            </ReactFlashlight>
+          </div>
+        </div>
+
+        <div className="container">
+          <span>Use the <span style={{color: "red"}}>mouse</span> to move the light.</span>          
+          <span>You can turn on/off the lights.</span>
+          <button style={{width: "150px", margin: "20px"}} onClick={()=>setEnabled(!enabled)}>Turn on/off</button>
+          <ReactFlashlight enabled={enabled}>
             <div style={style}>
-              <span>Hello, I'm a child element.</span>
+              <img style={{width: "80vw", height: "auto"}} src={bg} alt="bg" />
             </div>
           </ReactFlashlight>
         </div>
@@ -52,11 +58,11 @@ function App() {
           <span>This is light is controlled programmatically.</span>
           <ReactFlashlight
             enableMouse={false}
-            wheelResize={false}
             showCursor={true}
-            moveTo={position}>
+            moveTo={position}
+            >
             <div style={style}>
-              <span>Hello, I'm controlled programmatically.</span>
+              <img style={{width: "80vw", height: "auto"}} src={bg} alt="bg" />
             </div>
           </ReactFlashlight>
         </div>
@@ -65,21 +71,12 @@ function App() {
           <span>This is light is controlled programmatically and by the mouse.</span>
           <ReactFlashlight moveTo={position}>
             <div style={style}>
-              <span>Hello, I'm controlled programmatically and by the mouse.</span>
+              <img style={{width: "80vw", height: "auto"}} src={bg} alt="bg" />
             </div>
           </ReactFlashlight>
         </div>
 
-        <div className="container">
-          <span>This is light can move outside of the container.</span>
-          <ReactFlashlight contain={false}>
-            <div style={style}>
-              <span>Hello, I'm controlled programmatically and by the mouse.</span>
-            </div>
-          </ReactFlashlight>
-        </div>
-
-        <span><a href="http://www.freepik.com">Background designed by brgfx / Freepik</a></span>
+        <span><a href="http://www.freepik.com">Image designed by brgfx / Freepik</a></span>
 
       </header>
     </div> 
